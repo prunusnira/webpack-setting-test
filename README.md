5de55708d954b1e11d7ea8344f0342e3acaa4d2b
Nira <prunusnira@gmail.com>
2021-03-11 11:11:13 +0900

    init commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Babel+Webpack+TypeScript+ReactJS setting test
+* Check https://arincblossom.wordpress.com/2021/03/10/babel-webpack-%ea%b0%9c%eb%85%90%ea%b3%bc-%ed%99%98%ea%b2%bd-%ea%b5%ac%ec%b6%95/
---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.tsx',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: 'ts-loader'
+            },
+            {
+                test: /\.html$/,
+                use: {
+                    loader: 'html-loader',
+                    options: {
+                        minimize: true
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            //favicon: 'public/favicon.ico'
+        }),
+        new CleanWebpackPlugin()
+    ],
+    devServer: {
+        host: 'localhost',
+        port: 3000,
+        open: true
+    },
+    resolve: {
+        extensions: ['.wasm', '.js', '.json', '.ts', '.tsx']
+    }
+};
